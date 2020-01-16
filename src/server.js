ffeb746265f8e85efe03d387ae998d6988cbc23e
nirkaufman <nirkaufman@gmail.com>
2020-01-15 23:51:23 -0500

    update members data

--- src/server.js
+++ src/server.js
@@ -87,30 +87,32 @@ server.post('/auth/login', (req, res) => {
   }
 });
 
-server.use(/^(?!\/auth).*$/, (req, res, next) => {
-  if(req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-    const status = 401;
-    const message = 'Error in authorization format';
-    res.status(status).json({status, message});
-    return
-  }
-  try {
-    let verifyTokenResult;
-    verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+//todo: UNCOMMENT TO ENABLE AUTH ROUTES
 
-    if(verifyTokenResult instanceof Error) {
-      const status = 401;
-      const message = 'Access token not provided';
-      res.status(status).json({status, message});
-      return
-    }
-    next()
-  } catch (err) {
-    const status = 401;
-    const message = 'Error access_token is revoked';
-    res.status(status).json({status, message})
-  }
-});
+// server.use(/^(?!\/auth).*$/, (req, res, next) => {
+//   if(req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+//     const status = 401;
+//     const message = 'Error in authorization format';
+//     res.status(status).json({status, message});
+//     return
+//   }
+//   try {
+//     let verifyTokenResult;
+//     verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+//
+//     if(verifyTokenResult instanceof Error) {
+//       const status = 401;
+//       const message = 'Access token not provided';
+//       res.status(status).json({status, message});
+//       return
+//     }
+//     next()
+//   } catch (err) {
+//     const status = 401;
+//     const message = 'Error access_token is revoked';
+//     res.status(status).json({status, message})
+//   }
+// });
 
 server.use(router);
 
